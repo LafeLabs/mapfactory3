5892d5541376e7994fc32cbc5a4d86bae4c30bcc
Lafe Spietz <lafelabs@gmail.com>
2018-11-10 00:41:06 -0500

    replication of meme factory inside map factory

--- dnagenerator.php
+++ dnagenerator.php
@@ -44,6 +44,7 @@ $finalstring .= getALLfiles("mapicons");
 $finalstring .= ",\n";
 $finalstring .= getALLfiles("icons");
 $finalstring .= ",\n";
+$finalstring .= "    {\n        \"path\":\"memefactory\",\n        \"files\":[\n            \"replicator.php\"\n        ]\n    },\n";
 $finalstring .= "    {\n        \"path\":\"symbol\",\n        \"files\":[\n            \"replicator.php\"\n        ]\n    },\n";
 $finalstring .= "    {\n        \"path\":\"curve\",\n        \"files\":[\n            \"replicator.php\"\n        ]\n    },\n";
 $finalstring .= "    {\n        \"path\":\"scroll\",\n        \"files\":[\n            \"replicator.php\"\n        ]\n    }";
