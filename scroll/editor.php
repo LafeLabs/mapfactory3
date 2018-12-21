0db73ecb8c22f4613caae3f6f7bf08e6434afc40
Lafe Spietz <lafelabs@gmail.com>
2018-12-21 02:19:54 -0500

    tree dna construction for scroll with latex and image dirs

--- scroll/editor.php
+++ scroll/editor.php
@@ -24,6 +24,7 @@ EGO DEATH:
 <div id = "linkscroll">
     <a href = "text2php.php">text2php.php</a>
     <a href = "dnagenerator.php">dnagenerator.php</a>    
+    <a href = "treednagenerator.php">treednagenerator.php</a>    
     <a href = "tree.php">tree.php</a>    
     <a href = "index.php">index.php</a>
     <a href = "texeditor.php">texeditor.php</a>
@@ -42,12 +43,16 @@ EGO DEATH:
     <div class = "php file">php/fileloader.txt</div>
     <div class = "php file">php/text2php.txt</div>
     <div class = "php file">php/tree.txt</div>
+    <div class = "php file">php/treereplicator.txt</div>
+    <div class = "php file">php/treednagenerator.txt</div>
 
     <div class = "php file">php/texeditor.txt</div>
     <div class = "php file">php/copy.txt</div>
     <div class = "php file">php/mkdir.txt</div>
 
     <div class = "json file">json/dna.txt</div>
+    <div class = "json file">json/treedna.txt</div>
+
 </div>
 
 <script>
