10a07dcacb21306fc348f67d4ef117e3cb612734
Lafe Spietz <lafelabs@gmail.com>
2019-02-26 23:46:32 -0500

    memefeed still not working, but some parts of it work

--- memefeed.php
+++ memefeed.php
@@ -87,8 +87,8 @@ echo $listtext;
 <a href = "index.php" id = "backlink">BACK</a>
 <div id = "imagescroll"></div>
 
-<img id = "bottomimage" style = "display:none"/>
-<img id = "topimage" style = "display:none"/>
+<img id = "bottomImage" style = "display:none"/>
+<img id = "topImage" style = "display:none"/>
 
 <canvas id = "mainCanvas"></canvas>
 
@@ -96,64 +96,115 @@ echo $listtext;
 <div class = "bar" id = "rotatebar">R O T A T E</div>
     
 <script>
-
-    document.getElementById("mainCanvas").width = 0.65*innerWidth;
-    document.getElementById("mainCanvas").height = innerHeight - 250;
     
+
+imgurls = JSON.parse(document.getElementById("imgurls").innerHTML);
+mapicons = document.getElementById("mapicons").innerHTML.split(",");
+uploadimages = document.getElementById("uploadimages").innerHTML.split(",");
+symbols = document.getElementById("symbols").innerHTML.split(",");
+curves = document.getElementById("curves").innerHTML.split(",");    
+for(var index = 0;index < uploadimages.length - 1;index++){
+    imgurls.push("uploadimages/" + uploadimages[index]);
+}
+    
+for(var index = 0;index < symbols.length - 1;index++){
+    imgurls.push(symbols[index]);
+}
+for(var index = 0;index < curves.length - 1;index++){
+    imgurls.push("curve/svg/" + curves[index]);
+}
+for(var index = 0;index < mapicons.length - 1;index++){
+    imgurls.push("mapicons/" + mapicons[index]);
+}
+    
+for(var index = 0;index < imgurls.length; index++){
+    var newimg = document.createElement("IMG");
+    newimg.src = imgurls[index];
+    newimg.className = "button";
+    document.getElementById("imagescroll").appendChild(newimg);
+    newimg.onclick = function(){
+        currentImage = this.src;
+        if(imageLayer = 0){
+            bottomImage = currentImage;
+            document.getElementById("bottomImage").src = currentImage;
+            imageLayer = 1;
+            redraw();
+        }
+        else{
+            topImage = currentImage;
+            document.getElementById("topImage").src = currentImage;
+            redraw();
+        }
+    }
+}
+   
+    
+init();
+function init(){
+    W = 0.65*innerWidth;
+    H = innerHeight - 250;
+    document.getElementById("mainCanvas").width = W;
+    document.getElementById("mainCanvas").height = H;
+        
     bottomImage = "";
     topImage = "";
     currentImage = "";
     imageLayer = 0;
 
-    imgurls = JSON.parse(document.getElementById("imgurls").innerHTML);
-    mapicons = document.getElementById("mapicons").innerHTML.split(",");
-    uploadimages = document.getElementById("uploadimages").innerHTML.split(",");
-    symbols = document.getElementById("symbols").innerHTML.split(",");
-    curves = document.getElementById("curves").innerHTML.split(",");    
-    for(var index = 0;index < uploadimages.length - 1;index++){
-        imgurls.push("uploadimages/" + uploadimages[index]);
-    }
+    x0 = 0;
+    y0 = 0;
+    x1 = 0;
+    y1 = 0;
+    w0 = innerWidth*0.65;
+    ar0 = 1;
+    w1 = innerWidth*0.65;
+    ar1 = 1;
+    theta0 = 0;
+    theta1 = 0;
+
+    canvas = document.getElementById("mainCanvas");
+    ctx = canvas.getContext("2d");
     
-    for(var index = 0;index < symbols.length - 1;index++){
-        imgurls.push(symbols[index]);
-    }
-    for(var index = 0;index < curves.length - 1;index++){
-        imgurls.push("curve/svg/" + curves[index]);
-    }
-    for(var index = 0;index < mapicons.length - 1;index++){
-        imgurls.push("mapicons/" + mapicons[index]);
-    }
+}
+
+document.getElementById("bottomImage").onload = function(){
+    ar0 = this.height/this.width;
+    redraw();
+}
+document.getElementById("topImage").onload = function(){
+    ar1 = this.height/this.width;
+    redraw();
+}
+
+function redraw(){
+    ctx.clearRect(0,0,W,H);
+
+    ctx.translate(x0,y0);
+    ctx.rotate(theta0);
+    ctx.drawImage(document.getElementById("bottomImage"),0,0,w0,w0*ar0);
+    ctx.rotate(-theta0);
+    ctx.translate(-x0,-y0);
+
+    ctx.translate(x1,y1);
+    ctx.rotate(theta1);
+    ctx.drawImage(document.getElementById("topImage"),0,0,w1,w0*ar1);    
+    ctx.rotate(-theta1);
+    ctx.translate(-x1,-y1);
     
 
+}
 
-    for(var index = 0;index < imgurls.length; index++){
-        var newimg = document.createElement("IMG");
-        newimg.src = imgurls[index];
-        newimg.className = "button";
-        document.getElementById("imagescroll").appendChild(newimg);
-        newimg.onclick = function(){
-            currentImage = this.src;
-
-        }
-    }
-    
-    
-/*
-
-mc = new Hammer(document.getElementById("pagebox"));
+mc = new Hammer(document.getElementById("mainCanvas"));
 mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 mc.on("panleft panright panup pandown tap press", function(ev) {
 
-    map[map.length - 1].x = (0.5*w + ev.deltaX)/w;
-    map[map.length - 1].y = (0.25*w + ev.deltaY)/w;
-    
-    document.getElementById("a" + (map.length - 1).toString()).style.left = (0.5*w + ev.deltaX).toString() + "px";
-    document.getElementById("a" + (map.length - 1).toString()).style.top = (0.25*w + ev.deltaY).toString() + "px";
+    x0 = ev.deltaX;
+    y0 = ev.deltaY;
+    console.log(x0);
+    redraw();
 
 });    
 
-*/
-    
 </script>
 <style>
 
@@ -196,7 +247,7 @@ body{
     position:absolute;
     right:0px;
     top:100px;
-    z-index:-1;
+    z-index:1;
 }
 .bar{
     position:absolute;
