78974bc81db6d3a9cac8bfec21a56ac6e3efc85e
Lafe Spietz <lafelabs@gmail.com>
2019-03-10 21:10:12 -0400

    fixed image file names in get scroll php script

--- scroll/getscroll.php
+++ scroll/getscroll.php
@@ -37,7 +37,8 @@ foreach($foo as $value){
     if($index > 0){
         $imgurl = explode(")",$value)[0];
         copy($imgurl,"bookfactory/".$newfilename."/image".strval($index).".png");
-        $scroll = str_replace("![](bookfactory/".$newfilename."/".$imgurl.")","![](image".strval($index).".png)",$scroll);
+        
+        $scroll = str_replace("![](".$imgurl.")","![](bookfactory/".$newfilename."/image".strval($index).".png)",$scroll);
         $indexhtml .= "<li><a href = \"image".strval($index).".png\"><img src = \"image".strval($index).".png\"/></a></li>\n";
     }
     $index++;
